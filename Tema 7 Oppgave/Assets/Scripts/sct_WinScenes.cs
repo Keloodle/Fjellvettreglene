07b10ed7d4261bc9fd7cfba64013adc7c4e5959d
Sebbe <sebbeblind@gmail.com>
2021-04-20 18:50:22 +0200

    Adder ending scenene.
    Scenene mangler fungerender script på credits knappene.

---
+++ Tema 7 Oppgave/Assets/Scripts/sct_WinScenes.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sct_WinScenes : MonoBehaviour
+{
+
+
+    public void GoodEnding()
+    {
+        SceneManager.LoadScene("Blank");
+    }
+
+    public void MediumEnding()
+    {
+        SceneManager.LoadScene("Blank");
+    }
+
+    public void BadEnd()
+    {
+        SceneManager.LoadScene("Blank");
+    }
+
+
+}
