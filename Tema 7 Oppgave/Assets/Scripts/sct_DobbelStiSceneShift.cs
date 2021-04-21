1451906240c0e55d6bc2a0b60a66df505e77d5c9
Sebbe <sebbeblind@gmail.com>
2021-04-21 23:45:58 +0200

    Legger til alle dag/ettermiddag/natt scenene.
    Alle scenene mangler transition knapper og lydeffekter.

--- Tema 7 Oppgave/Assets/Scripts/sct_DobbelStiSceneShift.cs
+++ Tema 7 Oppgave/Assets/Scripts/sct_DobbelStiSceneShift.cs
@@ -8,10 +8,28 @@ public class sct_DobbelStiSceneShift : MonoBehaviour
 
     public void TiKilometerSkilt()
     {
-        SceneManager.LoadScene("blank");
+        SceneManager.LoadScene(5);
     }
 
     public void FemKilometerSkilt()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void FemKilometerNoon()
+    {
+        SceneManager.LoadScene("blank");
+    }    
+    
+    public void TiKilometerNoon()
+    {
+        SceneManager.LoadScene("blank");
+    }
+    public void FemKilometerNatt()
+    {
+        SceneManager.LoadScene("blank");
+    }
+    public void TiKilometerNatt()
     {
         SceneManager.LoadScene("blank");
     }
